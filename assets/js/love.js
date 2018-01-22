3b149ddf6c20b9ffcb9a7e06cf0e01162dba13d6
Matteo Paonessa <ardell.alarion@gmail.com>
2018-01-22 23:34:37 +0100

    Queue

--- assets/js/love.js
+++ assets/js/love.js
@@ -1,33 +1,80 @@
 function playIntro() {
-    var phrases = ['If you are gonna make it to the Moon', 'I\'ll be right there by your side', 'System initializing...'];
+    var phrases = [
+        // 'If you are gonna make it to the Moon',
+        // 'I\'ll be right there by your side',
+        'System booting',
+        'Initializing audio channels',
+        'Generating visual effects',
+        'Entering queue'
+    ];
 
     var el = document.querySelector('#init-text');
     var fx = new TextScramble(el);
 
     var counter = 0;
     var loops = 0;
+    $('#loading-progress').css('width', '100%');
     var next = function next() {
-    	if (loops === 1) {
-    	    playError();
-    		return false;
-		}
+        if (loops === 1) {
+            playError();
+            return false;
+        }
         fx.setText(phrases[counter]).then(function () {
             setTimeout(next, 2000);
         });
-    	if (counter === phrases.length - 1) {
-    	    setTimeout(function () {
-                $('#loading-progress').css('width', '100%');
-            }, 2500);
-
+        switch (counter) {
+            case 0:
+                break;
+            case 1:
+                audioController.play();
+                break;
         }
         counter = (counter + 1) % phrases.length;
         if (counter === 0) {
-        	loops++;
-		}
+            loops++;
+        }
     };
     next();
 }
 
 function playError() {
+
+}
+
+var audioController = {
+    audioElem: $('#background-audio').get(0),
+
+    play: function () {
+        var ctx = new AudioContext();
+
+
+        var audioSrc = ctx.createMediaElementSource(audioController.audioElem);
+        var analyser = ctx.createAnalyser();
+        analyser.connect(ctx.destination);
+        audioSrc.connect(analyser);
+        analyser.fftSize = 32;
+        var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+        function renderFrame() {
+            requestAnimationFrame(renderFrame);
+            analyser.getByteFrequencyData(frequencyData);
+            $('.audio-bars > div').each(function () {
+                $(this).css('height', Math.max(frequencyData[$(this).index()] * 20 / 255), 1);
+            });
+        }
+        audioController.audioElem.volume = 0.2;
+        audioController.audioElem.play();
+        renderFrame();
+    },
+    toggle: function () {
+        if (audioController.audioElem.paused) {
+            audioController.audioElem.play();
+        } else {
+            audioController.audioElem.pause();
+        }
+    }
+};
+
+function playAudio() {
 
 }
